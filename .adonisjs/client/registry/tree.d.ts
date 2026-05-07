e0e4fba0eb0681c45bae145dc649e50d68b4155f
st47k4 <salim.alarcon@protonmail.com>
2026-05-07 14:12:36 +0200

    fix: remove .adonisjs from tracking, ignore entire directory

--- .adonisjs/client/registry/tree.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-import type { routes } from './index.ts'
-
-export interface ApiDefinition {
-  home: typeof routes['home']
-  learn: typeof routes['learn']
-}
